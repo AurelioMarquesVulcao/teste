78835b0d0a3e7484caa658ca001ea92023def3a5
Aurelio Vulcão <vulcao@vulcaotech.com>
2020-01-30 09:29:29 -0300

    site?

---
+++ build/precache-manifest.4a639dce415b5b9da992829e505a1c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdacac08d6aa6baaca055fa82b051ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16402ff4233003f627f4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "acd9b53134e434c328b5",
+    "url": "/static/js/2.16693d2c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.16693d2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16402ff4233003f627f4",
+    "url": "/static/js/main.c58db361.chunk.js"
+  },
+  {
+    "revision": "eaddffa659834a19d723",
+    "url": "/static/js/runtime-main.3d2e3eea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
